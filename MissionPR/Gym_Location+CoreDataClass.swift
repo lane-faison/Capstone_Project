994d37ccf6ef7b2df11e776bb318590bd3e4d121
Lane Faison <lane.faison@icloud.com>
2017-05-31 16:38:19 -0600

    Prints gym name after main menu fetch

---
+++ MissionPR/Gym_Location+CoreDataClass.swift
@@ -0,0 +1,14 @@
+//
+//  Gym_Location+CoreDataClass.swift
+//  MissionPR
+//
+//  Created by Lane Faison on 5/31/17.
+//  Copyright © 2017 Lane Faison. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+public class Gym_Location: NSManagedObject {
+
+}
