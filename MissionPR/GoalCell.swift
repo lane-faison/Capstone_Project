29e6372e09f16e6064ce0eb582d1f919d34c20b8
Lane Faison <lane.faison@icloud.com>
2017-06-01 01:28:42 -0600

    Goal cells appending to tableView

--- MissionPR/GoalCell.swift
+++ MissionPR/GoalCell.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class GoalCell: UITableViewCell {
 
-
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var weight: UILabel!
+    @IBOutlet weak var reps: UILabel!
+    @IBOutlet weak var progressView: UIProgressView!
+    
+    func configureCell(goalLift: Goal_Lift) {
+        name.text = goalLift.name
+        weight.text = "\(goalLift.weight) Lbs."
+        reps.text = "\(goalLift.reps)"
+        progressView.setProgress(0.5, animated: true)
+        progressView.progressTintColor = UIColor.blue
+    }
 
 }
