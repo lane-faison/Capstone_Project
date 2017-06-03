06daa57e010231aabc1cdb971d530a266e73b59d
Lane Faison <lane.faison@icloud.com>
2017-06-03 15:24:24 -0600

    GymCheckIn btn returns set gym's coordinates. Gym name label appears after being set.

--- MissionPR/RunGoalCell.swift
+++ MissionPR/RunGoalCell.swift
@@ -16,5 +16,28 @@ class RunGoalCell: UITableViewCell {
     @IBOutlet weak var current: UILabel!
     @IBOutlet weak var goal: UILabel!
     @IBOutlet weak var progressView: UIProgressView!
+    
+//    func configureProgress(goalRun: Goal_Run) {
+//        print("Configure process function...")
+//        
+//        let transform = CGAffineTransform(scaleX: 1.0, y: 8.0)
+//        self.progressView.transform = transform
+//        
+//        let progress = Float(goalRun.current)/Float(goalRun.weight)
+//        
+//        progressView.setProgress(Float(goalRun.current)/Float(goalRun.weight), animated: true)
+//        
+//        if progress <= 1.00 {
+//            progressView.progressTintColor = UIColor(red: 76/255, green: 217/255, blue: 100/255, alpha: 1.0)
+//        }
+//        if progress <= 0.85 {
+//            progressView.progressTintColor = UIColor(red: 255/255, green: 149/255, blue: 0/255, alpha: 1.0)
+//        }
+//        if progress <= 0.70 {
+//            progressView.progressTintColor = UIColor(red: 244/255, green: 67/255, blue: 54/255, alpha: 1.0)
+//        }
+//        
+//        
+//    }
 
 }
