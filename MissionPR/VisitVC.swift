30afa7efce50696bcf9a8c04f76e1dde7f4cfc39
Lane Faison <lane.faison@icloud.com>
2017-06-07 12:54:43 -0600

    Visit progress bar working

--- MissionPR/VisitVC.swift
+++ MissionPR/VisitVC.swift
@@ -14,14 +14,25 @@ class VisitVC: UIViewController, NSFetchedResultsControllerDelegate {
     var controller: NSFetchedResultsController<Gym_Visits>!
     var dayTrackerCount = Int()
     var daysInCurrentMonth = Int()
+    var nameOfMonth = String()
     
-    @IBOutlet weak var testLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMMM"
+        nameOfMonth = dateFormatter.string(from: Date() as Date)
         
-       updateDays()
+        let transform = CGAffineTransform(rotationAngle: CGFloat(3*Double.pi/2)).scaledBy(x: 1.0, y: 20.0)
+
+        progressBar.transform = transform
+        
+        updateDays()
+        configureProgress()
     }
 
     func attemptFetch() {
@@ -84,7 +95,25 @@ class VisitVC: UIViewController, NSFetchedResultsControllerDelegate {
         let range = calendar.range(of: .day, in: .month, for: date)!
         daysInCurrentMonth = range.count
         
-        testLabel.text = "\(dayTrackerCount)/\(daysInCurrentMonth)"
+        label1.text = "\(daysInCurrentMonth) days in \(nameOfMonth)"
+        label2.text = "\(dayTrackerCount) visits to a gym so far!"
+    }
+    
+    func configureProgress() {
+        
+        let progress = Float(dayTrackerCount)/Float(daysInCurrentMonth)
+        
+        progressBar.setProgress(Float(dayTrackerCount)/Float(daysInCurrentMonth), animated: true)
+        
+        if progress <= 1.00 {
+            progressBar.progressTintColor = UIColor(red: 76/255, green: 217/255, blue: 100/255, alpha: 1.0)
+        }
+        if progress <= 2/3 {
+            progressBar.progressTintColor = UIColor(red: 255/255, green: 149/255, blue: 0/255, alpha: 1.0)
+        }
+        if progress <= 1/3 {
+            progressBar.progressTintColor = UIColor(red: 244/255, green: 67/255, blue: 54/255, alpha: 1.0)
+        }
     }
 
 }
