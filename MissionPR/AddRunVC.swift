ce5c53cfcd3f5f324898990c60ebef13442d2dc3
Lane Faison <lane.faison@icloud.com>
2017-06-08 11:40:51 -0600

    Added Run table with run cells properly linked from generated Data. App debugged after crash on load. Needed to delete app since new CoreData entity was added.

--- MissionPR/AddRunVC.swift
+++ MissionPR/AddRunVC.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class AddRunVC: UIViewController {
 
+    var goalToEdit: Goal_Run?
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
--- MissionPR/RunsVC.swift
+++ MissionPR/RunsVC.swift
@@ -7,14 +7,182 @@
 //
 
 import UIKit
+import CoreData
 
-class RunsVC: UIViewController {
+class RunsVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFetchedResultsControllerDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var controller: NSFetchedResultsController<Goal_Run>!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        if let topItem = self.navigationController?.navigationBar.topItem {
+            topItem.backBarButtonItem = UIBarButtonItem(title: " ", style: UIBarButtonItemStyle.plain, target: nil, action: nil)
+        }
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+//        generateTestData()
+        attemptFetch()
     }
-
-
+    
+    //    override func viewWillAppear(_ animated: Bool) {
+    //        super.viewWillAppear(animated)
+    //        animateTable()
+    //    }
+    
+    func animateTable() {
+        tableView.reloadData()
+        let cells = tableView.visibleCells
+        
+        let tableViewHeight = tableView.bounds.size.height
+        //        let tableViewWidth = tableView.bounds.size.width
+        
+        for cell in cells {
+            cell.transform = CGAffineTransform(translationX: 0, y: tableViewHeight)
+        }
+        
+        var delayCounter = 0
+        for cell in cells {
+            UIView.animate(withDuration: 1.75, delay: Double(delayCounter) * 0.0, usingSpringWithDamping: 1.0, initialSpringVelocity: 0, options: .curveEaseInOut, animations: {
+                cell.transform = CGAffineTransform.identity
+            }, completion: nil)
+            delayCounter += 1
+        }
+    }
+    
+    func configureCell(cell: RunGoalCell, indexPath: NSIndexPath) {
+        // update cell
+        let goal = controller.object(at: indexPath as IndexPath)
+        cell.configureCell(goalRun: goal)
+        cell.configureProgress(goalRun: goal)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RunGoalCell", for: indexPath) as! RunGoalCell
+        configureCell(cell: cell, indexPath: indexPath as NSIndexPath)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let objs = controller.fetchedObjects , objs.count > 0 {
+            let goal = objs[indexPath.row]
+            performSegue(withIdentifier: "editGoal", sender: goal)
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "editGoal" {
+            if let destination = segue.destination as? AddRunVC {
+                if let goal = sender as? Goal_Run {
+                    destination.goalToEdit = goal
+                }
+            }
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let sections = controller.sections {
+            let sectionInfo = sections[section]
+            return sectionInfo.numberOfObjects
+        }
+        return 0
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        if let sections = controller.sections {
+            return sections.count
+        }
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
+    
+    func attemptFetch() {
+        let fetchRequest: NSFetchRequest<Goal_Run> = Goal_Run.fetchRequest()
+        let runSort = NSSortDescriptor(key: "name", ascending: true)
+        fetchRequest.sortDescriptors = [runSort]
+        
+        let controller = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
+        
+        controller.delegate = self
+        self.controller = controller
+        
+        do {
+            try self.controller.performFetch()
+        } catch {
+            let error = error as NSError
+            print("\(error)")
+        }
+    }
+    
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        switch(type) {
+        case.insert:
+            if let indexPath = newIndexPath {
+                tableView.insertRows(at: [indexPath], with: .fade)
+            }
+            break
+        case.delete:
+            if let indexPath = indexPath {
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+            break
+        case.update:
+            if let indexPath = indexPath {
+                let cell = tableView.cellForRow(at: indexPath) as! RunGoalCell
+                // update the cell data
+                configureCell(cell: cell, indexPath: indexPath as NSIndexPath)
+            }
+            break
+        case.move:
+            if let indexPath = indexPath {
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+            if let indexPath = newIndexPath {
+                tableView.insertRows(at: [indexPath], with: .fade)
+            }
+            break
+        }
+    }
+    
+    func generateTestData() {
+        let goal1 = Goal_Run(context: context)
+        goal1.name = "3 Mile Run"
+        goal1.currentTime = 1200
+        goal1.goalTime = 1100
+        goal1.distance = 3
+        goal1.units = "mi"
+        
+        let goal2 = Goal_Run(context: context)
+        goal2.name = "1 Mile Run"
+        goal2.currentTime = 500
+        goal2.goalTime = 450
+        goal2.distance = 1
+        goal2.units = "mi"
+        
+        let goal3 = Goal_Run(context: context)
+        goal3.name = "BolderBoulder"
+        goal3.currentTime = 3600
+        goal3.goalTime = 3000
+        goal3.distance = 10
+        goal3.units = "km"
+        
+        ad.saveContext()
+    }
+    
 }
+
