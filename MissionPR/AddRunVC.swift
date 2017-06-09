32f885627b176e5b5a709d7384a1acd76e3853f8
Lane Faison <lane.faison@icloud.com>
2017-06-08 23:41:26 -0600

    Correct run data appearing after edit segue

--- MissionPR/AddRunVC.swift
+++ MissionPR/AddRunVC.swift
@@ -22,7 +22,7 @@ class AddRunVC: UIViewController, UITextFieldDelegate, UIPickerViewDelegate, UIP
     
     var goalToEdit: Goal_Run?
     
-    var distanceDataSource = [[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30],["mi","km"]]
+    var distanceDataSource = [[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30],[0,1]]
     
     var timeDataSource = [[0,1,2,3,4,5],[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59],[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59]]
     
@@ -39,6 +39,8 @@ class AddRunVC: UIViewController, UITextFieldDelegate, UIPickerViewDelegate, UIP
         currentPicker.dataSource = self
         goalPicker.delegate = self
         goalPicker.dataSource = self
+        
+        loadGoalData()
         
         if goalToEdit != nil {
             loadGoalData()
@@ -78,10 +80,22 @@ class AddRunVC: UIViewController, UITextFieldDelegate, UIPickerViewDelegate, UIP
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
         if pickerView == distancePicker {
+            
             let choice = "\(distanceDataSource[component][row])"
+            
+//            if distanceDataSource[1][row] == 0 {
+//                choice = "mi"
+//            }
+//            
+//            if distanceDataSource[1][row] == 1 {
+//                choice = "km"
+//            }
+            
             return choice
         }
+        
         if pickerView == currentPicker {
             if component == 0 {
                 let choice = "\(timeDataSource[component][row])h"
@@ -121,14 +135,50 @@ class AddRunVC: UIViewController, UITextFieldDelegate, UIPickerViewDelegate, UIP
         if let runGoal = goalToEdit {
             nameTextField.text = runGoal.name
             
-//            let indexOfDistance = distanceDataSource[0].index
-//                .index(of: Int(runGoal.distance))
-//            let indexOfUnits =
-//            let indexOfCurrent = current.index(of: Int(liftGoal.current))
-//            let indexOfGoal = goal.index(of: Int(liftGoal.weight))
-//            repsPicker.selectRow(indexOfReps!, inComponent: 0, animated: true)
-//            currentPicker.selectRow(indexOfCurrent!, inComponent: 0, animated: true)
-//            goalPicker.selectRow(indexOfGoal!, inComponent: 0, animated: true)
+            let indexOfDistance = distanceDataSource[0].index(of: Int(runGoal.distance))
+            distancePicker.selectRow(indexOfDistance!, inComponent: 0, animated: true)
+            
+            if runGoal.units == "mi" {
+                distancePicker.selectRow(0, inComponent: 1, animated: true)
+            }
+            if runGoal.units == "km" {
+                distancePicker.selectRow(1, inComponent: 1, animated: true)
+            }
+            
+            
+            let h_current = runGoal.currentTime/3600
+            let m_current = (runGoal.currentTime%3600)/60
+            let s_current = (runGoal.currentTime%3600)%60
+            
+            print(h_current)
+            print(m_current)
+            print(s_current)
+            
+            let h_goal = runGoal.goalTime/3600
+            let m_goal = (runGoal.goalTime%3600)/60
+            let s_goal = (runGoal.goalTime%3600)%60
+            
+            print(h_goal)
+            print(m_goal)
+            print(s_goal)
+            
+            let indexOfCurrent_h = timeDataSource[0].index(of: Int(h_current))
+            let indexOfCurrent_m = timeDataSource[1].index(of: Int(m_current))
+            let indexOfCurrent_s = timeDataSource[2].index(of: Int(s_current))
+            
+            let indexOfGoal_h = timeDataSource[0].index(of: Int(h_goal))
+            let indexOfGoal_m = timeDataSource[1].index(of: Int(m_goal))
+            let indexOfGoal_s = timeDataSource[2].index(of: Int(s_goal))
+            
+            currentPicker.selectRow(indexOfCurrent_h!, inComponent: 0, animated: true)
+            currentPicker.selectRow(indexOfCurrent_m!, inComponent: 1, animated: true)
+            currentPicker.selectRow(indexOfCurrent_s!, inComponent: 2, animated: true)
+            
+            goalPicker.selectRow(indexOfGoal_h!, inComponent: 0, animated: true)
+            goalPicker.selectRow(indexOfGoal_m!, inComponent: 1, animated: true)
+            goalPicker.selectRow(indexOfGoal_s!, inComponent: 2, animated: true)
+           
+
         }
     }
     
