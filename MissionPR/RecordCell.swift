94d3b5a4835eaa51035799f60e9a3286cc0440a6
Lane Faison <lane.faison@icloud.com>
2017-06-18 23:49:47 -0600

    RecordsVC set up. Record_Lift model set up.

---
+++ MissionPR/RecordCell.swift
@@ -0,0 +1,13 @@
+//
+//  RecordCell.swift
+//  MissionPR
+//
+//  Created by Lane Faison on 6/18/17.
+//  Copyright © 2017 Lane Faison. All rights reserved.
+//
+
+import UIKit
+
+class RecordCell: UICollectionViewCell {
+    
+}
