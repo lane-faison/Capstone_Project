04c0da2489cd2e4ebb536b80846b6f4e28d0d628
Lane Faison <lane.faison@icloud.com>
2017-10-01 23:45:23 -0600

    Code cleanup

--- MissionPR/AddLiftVC.swift
+++ MissionPR/AddLiftVC.swift
@@ -17,19 +17,39 @@ class AddLiftVC: UIViewController, UITextFieldDelegate, UIPickerViewDelegate, UI
     @IBOutlet weak var step4Label: UILabel!
     
     @IBOutlet weak var nameTextField: UITextField!
-    let limitLength = 22 //character length for above text field
-    @IBOutlet weak var errorLabel: UILabel!
-    @IBOutlet weak var repsPicker: UIPickerView!
-    @IBOutlet weak var goalPicker: UIPickerView!
-    @IBOutlet weak var currentPicker: UIPickerView!
+    private let limitLength = 22 //character length for above text field
+    @IBOutlet weak var errorLabel: UILabel! {
+        didSet {
+            errorLabel.isHidden = true
+        }
+    }
+    
+    @IBOutlet weak var repsPicker: UIPickerView! {
+        didSet {
+            repsPicker.selectRow(0, inComponent: 0, animated: true)
+        }
+    }
+    
+    @IBOutlet weak var goalPicker: UIPickerView! {
+        didSet {
+            goalPicker.selectRow(19, inComponent: 0, animated: true)
+        }
+    }
+    
+    @IBOutlet weak var currentPicker: UIPickerView! {
+        didSet {
+            goalPicker.selectRow(19, inComponent: 0, animated: true)
+        }
+    }
+    
     @IBOutlet weak var submitBtn: RoundedOutlineButton!
     @IBOutlet weak var deleteBtn: UIBarButtonItem!
     
-    var goalToEdit: Goal_Lift?
+    private var goalToEdit: Goal_Lift?
     
-    var reps = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-    var goal = [5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100,105,110,115,120,125,130,135,140,145,150,155,160,165,170,175,180,185,190,195,200,205,210,215,220,225,230,235,240,245,250,255,260,265,270,275,280,285,290,295,300,305,310,315,320,325,330,335,340,345,350,355,360,365,370,375,380,385,390,395,400,405,410,415,420,425,430,435,440,445,450,455,460,465,470,475,480,485,490,495,500]
-    var current = [5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100,105,110,115,120,125,130,135,140,145,150,155,160,165,170,175,180,185,190,195,200,205,210,215,220,225,230,235,240,245,250,255,260,265,270,275,280,285,290,295,300,305,310,315,320,325,330,335,340,345,350,355,360,365,370,375,380,385,390,395,400,405,410,415,420,425,430,435,440,445,450,455,460,465,470,475,480,485,490,495,500]
+    private var reps = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+    private var goal = [5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100,105,110,115,120,125,130,135,140,145,150,155,160,165,170,175,180,185,190,195,200,205,210,215,220,225,230,235,240,245,250,255,260,265,270,275,280,285,290,295,300,305,310,315,320,325,330,335,340,345,350,355,360,365,370,375,380,385,390,395,400,405,410,415,420,425,430,435,440,445,450,455,460,465,470,475,480,485,490,495,500]
+    private var current = [5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100,105,110,115,120,125,130,135,140,145,150,155,160,165,170,175,180,185,190,195,200,205,210,215,220,225,230,235,240,245,250,255,260,265,270,275,280,285,290,295,300,305,310,315,320,325,330,335,340,345,350,355,360,365,370,375,380,385,390,395,400,405,410,415,420,425,430,435,440,445,450,455,460,465,470,475,480,485,490,495,500]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -45,12 +65,6 @@ class AddLiftVC: UIViewController, UITextFieldDelegate, UIPickerViewDelegate, UI
         goalPicker.dataSource = self
         currentPicker.delegate = self
         currentPicker.dataSource = self
-        
-        errorLabel.isHidden = true
-        
-        repsPicker.selectRow(0, inComponent: 0, animated: true)
-        goalPicker.selectRow(19, inComponent: 0, animated: true)
-        currentPicker.selectRow(19, inComponent: 0, animated: true)
         
         super.viewDidLoad()
         self.hideKeyboardWhenTappedAround()
@@ -71,40 +85,33 @@ class AddLiftVC: UIViewController, UITextFieldDelegate, UIPickerViewDelegate, UI
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        if pickerView == repsPicker {
-            let choice = "\(reps[row])"
-            return choice
+        switch pickerView {
+        case repsPicker:
+            return "\(reps[row])"
+        case goalPicker:
+            return "\(goal[row])"
+        case currentPicker:
+            return "\(current[row])"
+        default:
+            return nil
         }
-        if pickerView == goalPicker {
-            let choice = "\(goal[row])"
-            return choice
-        }
-        if pickerView == currentPicker {
-            let choice = "\(current[row])"
-            return choice
-        }
-        return "nil"
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        if pickerView == repsPicker {
+        switch pickerView {
+        case repsPicker:
             return reps.count
-        }
-        if pickerView == goalPicker {
+        case goalPicker:
             return goal.count
-        }
-        if pickerView == currentPicker {
+        case currentPicker:
             return current.count
+        default:
+            return 1
         }
-        return 1
     }
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        //update when selected
     }
     
     @IBAction func addPressed(_ sender: Any) {
@@ -170,9 +177,6 @@ class AddLiftVC: UIViewController, UITextFieldDelegate, UIPickerViewDelegate, UI
             errorLabel.isHidden = false
             errorLabel.text = "* Please give this goal a name"
         }
-        else {
-            //
-        }
     }
     
     @IBAction func btnTouchDown(_ sender: UIButton) {
@@ -196,8 +200,8 @@ class AddLiftVC: UIViewController, UITextFieldDelegate, UIPickerViewDelegate, UI
     }
     
     @IBAction func deletePressed(_ sender: Any) {
-        if goalToEdit != nil {
-            context.delete(goalToEdit!)
+        if let goalToEdit = goalToEdit {
+            context.delete(goalToEdit)
             ad.saveContext()
         }
         navigationController?.popViewController(animated: true)
